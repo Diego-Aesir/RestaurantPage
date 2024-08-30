537a405396c90dd0c500eeb52d3645c9cef5fedb
Diego-Aesir <diego.s.galvao91@gmail.com>
2024-08-30 18:11:21 -0400

    Restaurant page: - The scope is done. - The idea behind the process is defined.

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+const nav = document.querySelector("nav"); 
+const content = document.querySelector("#content");
+
+const button_home = document.createElement("button");
+button_home.innerText = "Home";
+
+const button_menu = document.createElement("button");
+button_menu.innerText = "Menu";
+
+const button_contact = document.createElement("button");
+button_contact.innerText = "Contact";
+
+
+nav.appendChild(button_home);
+nav.appendChild(button_menu);
+nav.appendChild(button_contact);
+
+
+
+
+
+//Basicamente, a cada click, Home-Menu-Contact vai criar uma div e sera inserido la naquele content
+// Vai ter que ficar atualizando a parada e colocando a propria view.
+// Imagino que cada um tera seu css como padrao
+
